dfeec2ef6ec0679c5586cb5fbfaf2621c533355d
Megha Gulati <meggulat@publicisgroupe.net>
2018-06-13 16:10:50 +0530

    Project

--- rest/src/test/java/post.java
+++ rest/src/test/java/post.java
@@ -1,7 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.json.simple.JSONObject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -9,6 +12,59 @@ import static org.hamcrest.Matchers.equalTo;
 public class post {
 
     @Test
+    public void Testb()
+    {
+
+        RestAssured.baseURI = "http://restapi.demoqa.com/customer/register";
+
+
+        RequestSpecification req = RestAssured.given();
+
+        //create a JSON request that contain all fields
+
+        JSONObject reqParam = new JSONObject();
+        reqParam.put("FirstName","Megha");
+        reqParam.put("LastName","Gulati");
+        reqParam.put("UserName","mgula");
+        reqParam.put("Password","1234");
+        reqParam.put("Email","abc@gmail.com");
+
+        //add header in the request
+        req.header("Content-Type", "application/json");
+
+
+        //add json to the body of the request
+        req.body(reqParam.toJSONString());
+
+        //post the request and check the response
+        Response res = req.post("/register");
+
+        //validating the response
+        int statusCode = res.statusCode();
+        Assert.assertEquals(statusCode,200);
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Test(enabled = false)
     public  void Test2()
     {
         String b = "{"+
